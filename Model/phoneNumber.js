33261f11310558cb69f185bbc38877f6fa3385a7
Anand <anandcoder1@gmail.com>
2025-02-02 13:14:05 +0530

    Anand updated

--- Model/phoneNumber.js
+++ Model/phoneNumber.js
@@ -4,7 +4,6 @@ const phoneNumberschema = new mongoose.Schema ({
     phoneNumber: {type: String, required: true, unique: true},
     otp: {type: String, required: true},
     otpExpiration: {type: Date, required: true},
-    referralCode: {type: String, required: true}
 })
 
 module.exports= mongoose.model("phoneNumber" , phoneNumberschema);
