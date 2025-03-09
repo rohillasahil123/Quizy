ebd69ce8c9c1e00a2f41111ff0f9de78c8cb8aa2
Anand <anandcoder1@gmail.com>
2025-03-09 11:39:16 +0530

    middleware updated

--- Middelware/authenticateMiddleware.js
+++ Middelware/authenticateMiddleware.js
@@ -1,11 +1,17 @@
 const { getUser } = require("../services/auth");
 
 const ensureAuthenticated = (req, res, next) => {
-    const auth = req.cookies?.userToken;
-    if (!auth) return res.status(403).json({ message: "Unauthorized, JWT token is required" });
+    // const token = req.cookies?.userToken;
+    // if (!token) return res.status(403).json({ message: "Unauthorized, JWT token is required" });
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(403).json({ message: "Unauthorized, JWT token is required" });
+    }
+    const token = authHeader.split(" ")[1];
     
     try{
-        const decoded = getUser(auth);
+        const decoded = getUser(token);
         if (!decoded) return res.status(403).json({ message: "Unauthorized, JWT token is wrong" });
         req.user = decoded;
         next();
@@ -17,25 +23,3 @@ const ensureAuthenticated = (req, res, next) => {
 module.exports = {
     ensureAuthenticated,
 }
-
-
-// const ensureAuthenticated = (req, res, next) => {
-//     // const auth = req.cookies?.userToken;
-//     // if (!auth) return res.status(403).json({ message: "Unauthorized, JWT token is required" });
-    
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//         return res.status(403).json({ message: "Unauthorized, JWT token is required" });
-//     }
-//     const token = authHeader.split(" ")[1];
-
-//     try{
-//         const decoded = getUser(token);
-
-//         if (!decoded) return res.status(403).json({ message: "Unauthorized, JWT token is wrong" });
-//         req.user = decoded;
-//         next();
-//     } catch (error) {
-//         return res.status(403).json({ message: "Unauthorized, JWT token is wrong or expired" });
-//     }
-// }
